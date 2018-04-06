ab363bbed878bd041e335da41bc1ec48195de144
Łukasz Hryniuk <lukasz.hryniuk@wp.pl>
2018-04-07 01:12:29 +0200

    Fix user url

--- hacktomate/hacktomate/urls.py
+++ hacktomate/hacktomate/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(r'', include('hackathons.urls')),
-    path('user', include('user.urls')),
+    path('user/', include('user.urls')),
 ]
