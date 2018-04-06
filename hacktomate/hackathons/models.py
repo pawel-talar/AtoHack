a260b90d91a7733eaf660edd0d5cc6688bf578ca
marcin <kirinson321@gmail.com>
2018-04-07 00:02:33 +0200

    repair hackathons models.py

--- hacktomate/hackathons/models.py
+++ hacktomate/hackathons/models.py
@@ -2,13 +2,20 @@ from django.db import models
 
 class Tag:
     text = models.CharField(max_length=50)
-
-class Hackaton:
-    desc = models.TextField()
-    photo = models.ImageField(upload_to='photos', default='/photos/default.png')
-    tag = models.ManyToManyField(Tag)
     date_added = models.DateTimeField(auto_now_add=True)
-   # owner = models.ForeignKey(Organizer)
-   # contestants = models.ManyToManyField(Profile)
 
-# Create your models here.
+    def __str__(self):
+        return self.text
+
+
+
+class Hackathon:
+    desc = models.TextField()
+    #photo = models.ImageField(upload_to='photos', default='/photos/default.png')
+    #tag = models.ManyToManyField(Tag)
+    date_added = models.DateTimeField(auto_now_add=True)
+    #owner = models.ForeignKey(Organizer)
+    #contestants = models.ManyToManyField(Profile)
+
+    def __str__(self):
+        return self.desc
