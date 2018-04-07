d3d222c2b21621152ef271281d16235b6669c81b
Łukasz Hryniuk <lukasz.hryniuk@wp.pl>
2018-04-07 07:42:36 +0200

    Fix hackathons

--- hacktomate/hackathons/models.py
+++ hacktomate/hackathons/models.py
@@ -1,19 +1,6 @@
 from django.db import models
-<<<<<<< Updated upstream
 from organizer.models import Organizer
 from user.models import Profile
-=======
-#from hacktomate.organizer.models import Organizer
-#from hacktomate.user.models import Profile
->>>>>>> Stashed changes
-
-# class Tag:
-#     """model representing every available tag"""
-#     text = models.CharField(max_length=50)
-#     date_added = models.DateTimeField(auto_now_add=True)
-#
-#     def __str__(self):
-#         return self.text
 
 
 class Hackathon:
@@ -21,13 +8,8 @@ class Hackathon:
     text = models.TextField()
     photo = models.ImageField(upload_to='photos', default='/photos/default.png')
     date_added = models.DateTimeField(auto_now_add=True)
-<<<<<<< Updated upstream
     owner = models.ForeignKey(Organizer, on_delete=models.DO_NOTHING)
     contestants = models.ManyToManyField(Profile)
-=======
-#    owner = models.ForeignKey(Organizer)
-#    contestants = models.ManyToManyField(Profile)
->>>>>>> Stashed changes
 
     def __str__(self):
         return self.text
